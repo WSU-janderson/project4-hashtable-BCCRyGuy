5a27f5f4c7e411f90533528e3c093349784fc9d8
BCCRyGuy <cheetham.4@wright.edu>
2025-11-02 10:55:50 -0500

    generated definitions from header into cpp file

--- HashTable.cpp
+++ HashTable.cpp
@@ -5,3 +5,31 @@
 
 #include "HashTable.h"
 
+bool HashTable::insert(const std::string &key, const size_t &value) {
+
+}
+
+bool HashTable::remove(const std::string &key) {
+}
+
+bool HashTable::contains(const std::string &key) const {
+}
+
+std::optional<size_t> HashTable::get(const std::string &key) const {
+}
+
+size_t & HashTable::operator[](const std::string &key) {
+}
+
+std::vector<std::string> HashTable::keys() const {
+}
+
+double HashTable::alpha() const {
+}
+
+size_t HashTable::capacity() const {
+}
+
+size_t HashTable::size() const {
+}
+
