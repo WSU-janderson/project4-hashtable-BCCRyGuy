255cd27d642feffb8586a3231ca6bafb502d326a
BCCRyGuy <cheetham.4@wright.edu>
2025-11-02 12:08:01 -0500

    created constructors for bucket class

--- HashTableBucket.cpp
+++ HashTableBucket.cpp
@@ -4,3 +4,13 @@
  */
 
 #include "HashTableBucket.h"
+
+// default constructor
+HashTableBucket::HashTableBucket() : key("EMPTY_SINCE_START"), value(0), type(BucketType::ESS) {
+}
+
+// parameterized constructor
+HashTableBucket::HashTableBucket(const std::string &k, const size_t &v) : key(k), value(v), type(BucketType::NORMAL) {
+
+}
+
