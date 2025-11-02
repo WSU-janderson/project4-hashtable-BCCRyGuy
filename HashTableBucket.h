dc7ad8af4f98f2c9afc28fcaf7399b8ce0af1694
BCCRyGuy <cheetham.4@wright.edu>
2025-11-02 11:32:06 -0500

    fixed temporary considerations

--- HashTableBucket.h
+++ HashTableBucket.h
@@ -14,28 +14,28 @@ class HashTableBucket {
     * The default constructor can simply set the bucket type to ESS.
     *
     */
-    HashTableBucket::HashTableBucket();
+    HashTableBucket();
     /**
     * A parameterized constructor could initialize the key and value, as
     * well as set the bucket type to NORMAL.
     */
-    HashTableBucket::HashTableBucket(const std::string& key, const size_t& value);
+    HashTableBucket(const std::string& key, const size_t& value);
     /**
     * A load method could load the key-value pair into the bucket, which
     * should then also mark the bucket as NORMAL.
     */
-    void HashTableBucket::load(const std::string& key, const size_t& value)
+    void load(const std::string& key, const size_t& value);
     /**
     * This method would return whether the bucket is empty, regardless of
     * if it has had data placed in it or not.
     */
-    bool HashTableBucket::isEmpty() const
+    bool isEmpty() const;
     /**
     * The stream insertion operator could be overloaded to print the
     * bucket's contents. Or if preferred, you could write a print method
     * instead.
     */
-    friend ostream& operator<<(ostream& os, const HashTableBucket& bucket)
+    friend std::ostream& operator<<(std::ostream& os, const HashTableBucket& bucket);
 };
 
 
