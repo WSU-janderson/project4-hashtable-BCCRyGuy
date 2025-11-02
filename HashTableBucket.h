e5d0ce55270285ea68ff76975045bf7c2dca8368
BCCRyGuy <cheetham.4@wright.edu>
2025-11-02 11:40:09 -0500

    commented

--- HashTableBucket.h
+++ HashTableBucket.h
@@ -15,17 +15,14 @@ class HashTableBucket {
     public:
     // default constructor
     HashTableBucket();
+
     // parameterized constructor
     HashTableBucket(const std::string& key, const size_t& value);
-    /**
-    * A load method could load the key-value pair into the bucket, which
-    * should then also mark the bucket as NORMAL.
-    */
+
+    // load key value into bucket and mark as NORMAL
     void load(const std::string& key, const size_t& value);
-    /**
-    * This method would return whether the bucket is empty, regardless of
-    * if it has had data placed in it or not.
-    */
+
+    // check if empty (ESS or EAR)
     bool isEmpty() const;
     /**
     * The stream insertion operator could be overloaded to print the
