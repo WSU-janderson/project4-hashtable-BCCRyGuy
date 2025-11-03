803958bcca4451062d5068c5fbd5701ad49ad29e
BCCRyGuy <cheetham.4@wright.edu>
2025-11-02 19:38:30 -0500

    testing with current code. Test successful

--- HashTableDebug.cpp
+++ HashTableDebug.cpp
@@ -4,8 +4,23 @@
  * Write your tests in this file
  */
 #include <iostream>
+#include "HashTable.h"
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // default capacity 8
+    HashTable testTable;
+
+    // insert key-value pairs
+    testTable.insert("Rylan", 100);
+    testTable.insert("Greg", 237);
+    testTable.insert("Mumford", 891);
+
+    // print buckets
+    std:: cout << "Buckets after insertion:" << std::endl;
+    for (size_t i = 0; i < 8; i++ ) {
+        // table data moved to public temporarily
+        std::cout << "Bucket " << i << ": " << testTable.tableData[i];
+    }
     return 0;
 }
